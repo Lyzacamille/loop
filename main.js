e448245e7008f722e2be3893cf19edd8d80330f2
Lyza Norton <nextechcatapult@ELENA-APPLE-TV.localdomain>
2018-06-26 09:10:21 -0400

    Finsihed

---
+++ main.js
@@ -0,0 +1,73 @@
+// 1
+console.log("Display the numbers from 1 to 20.");
+for(var i = 1; i <= 20; i++) {
+   console.log(i + " ");{
+
+   }
+
+}
+// 2
+console.log("Display the even numbers from 1 to 20.");
+for( var i = 1; i <= 20; i++) {
+    if (i % 2 == 0){
+console.log(i +", ");}
+} 
+// 3
+console.log ("Display the odd numbers from 1 to 20. ");
+for (var i = 1; i <= 20; i++) {
+    if ( i % 2 !== 0){
+        console.log(i + "");{
+
+        }
+    }
+}
+// 4
+console.log("Dispaly multiples of 5 from 5 to 100");
+for (var i = 5; i<= 100; i++){
+    if (i % 5 ==0){
+        console.log(i + "");
+    }
+}
+// 5 
+console.log("Display square numbers up too 100");
+for (var i = 1; i<=10; i++){
+
+       console.log(i*i);
+   
+}
+
+// 6
+console.log("Display count down from 20 to 1");
+for (var i = 20; i >=1; i--){
+    if ( i > 0){
+        console.log(i - "")
+    }
+}
+// 7 
+console.log ("Display count down even numbers from 20 to 0");
+for (var i = 20; i>0;i--){
+    if (i % 2==0){
+        console.log (i-"")
+    }
+}
+// 8
+console.log("Display count down odd numbers from 20 to 0");
+for (var i = 20; i>0; i--){
+    if (i % 2 !== 0){
+        console.log(i-"");
+    }
+}
+
+// 9
+console.log("Display count down by 5 from 100 to 0 ");
+for (var i = 100; i>5; i--){
+    if (i % 5 ==0){
+        console.log(i-"");
+    }
+}
+// 10
+console.log("Display square numbers up too 100");
+for (var i = 10; i>1; i--){
+
+       console.log(i*i);}
+   
